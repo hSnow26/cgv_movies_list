7b3ac75116511598258c2c971cf362b682a98231
seolH <kshzg26@gmail.com>
2019-12-11 07:57:35 +0900

    handle crawler

---
+++ cgv_crawling_handler.py
@@ -0,0 +1,40 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+def init_driver(driver_path, headers):
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('window-size=1920x1080')
+    options.add_argument("disable-gpu")
+    options.add_argument("lang=ko_KR")
+    options.add_argument(headers)
+
+    driver = webdriver.Chrome(driver_path, options=options)
+    return driver
+
+def get_url_info(driver, url):
+    driver.get(url)
+    driver.execute_script("Object.defineProperty(navigator, 'plugins', {get: function() {return[1, 2, 3, 4, 5];},});")
+
+    # lanuages 속성을 업데이트해주기
+    driver.execute_script("Object.defineProperty(navigator, 'languages', {get: function() {return ['ko-KR', 'ko']}})")
+
+    #웹 자원 로드를 시간 기다림
+    driver.implicitly_wait(3)
+
+    html = driver.page_source
+
+    return html
+
+def get_text(html, selector):
+    soup = BeautifulSoup(html, 'lxml')
+    contents = soup.select(
+        # '#form1 > div.pop_th_wrap > div > div > div > div > div.th_preveiw_tab_popup > div.scroll > ul > li > a > span'
+        selector
+        )
+    content_list = []
+    for content in contents:
+        content_list.append(content.text)
+        print(content.text)
+
+    return content_list
