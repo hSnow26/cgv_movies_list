466c8065fba531792228e6d03006fad7ea9b25d6
hSnow <kshzg26@gmail.com>
2019-12-20 18:09:33 +0900

    create class

--- cgv_crawling_handler.py
+++ cgv_crawling_handler.py
@@ -1,40 +1,41 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-def init_driver(driver_path, headers):
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless')
-    options.add_argument('window-size=1920x1080')
-    options.add_argument("disable-gpu")
-    options.add_argument("lang=ko_KR")
-    options.add_argument(headers)
+class CgvCrawlingHandler:
+    def init_driver(self, driver_path, headers):
+        options = webdriver.ChromeOptions()
+        options.add_argument('headless')
+        options.add_argument('window-size=1920x1080')
+        options.add_argument("disable-gpu")
+        options.add_argument("lang=ko_KR")
+        options.add_argument(headers)
 
-    driver = webdriver.Chrome(driver_path, options=options)
-    return driver
+        self.driver = webdriver.Chrome(driver_path, options=options)
+        return self.driver
 
-def get_url_info(driver, url):
-    driver.get(url)
-    driver.execute_script("Object.defineProperty(navigator, 'plugins', {get: function() {return[1, 2, 3, 4, 5];},});")
+    def get_url_info(self, driver, url):
+        driver.get(url)
+        driver.execute_script("Object.defineProperty(navigator, 'plugins', {get: function() {return[1, 2, 3, 4, 5];},});")
 
-    # lanuages 속성을 업데이트해주기
-    driver.execute_script("Object.defineProperty(navigator, 'languages', {get: function() {return ['ko-KR', 'ko']}})")
+        # lanuages 속성을 업데이트해주기
+        driver.execute_script("Object.defineProperty(navigator, 'languages', {get: function() {return ['ko-KR', 'ko']}})")
 
-    #웹 자원 로드를 시간 기다림
-    driver.implicitly_wait(3)
+        #웹 자원 로드를 시간 기다림
+        driver.implicitly_wait(3)
 
-    html = driver.page_source
+        html = driver.page_source
 
-    return html
+        return html
 
-def get_text(html, selector):
-    soup = BeautifulSoup(html, 'lxml')
-    contents = soup.select(
-        # '#form1 > div.pop_th_wrap > div > div > div > div > div.th_preveiw_tab_popup > div.scroll > ul > li > a > span'
-        selector
-        )
-    content_list = []
-    for content in contents:
-        content_list.append(content.text)
-        print(content.text)
+    def get_text(self, html, selector):
+        soup = BeautifulSoup(html, 'lxml')
+        contents = soup.select(
+            # '#form1 > div.pop_th_wrap > div > div > div > div > div.th_preveiw_tab_popup > div.scroll > ul > li > a > span'
+            selector
+            )
+        content_list = []
+        for content in contents:
+            content_list.append(content.text)
+            print(content.text)
 
-    return content_list
+        return content_list
